9aec29f1b6ffb0fa924f44352498b6cba7ea8b31
الاء عبد السلام سيد محمد <abdomedo2033@gmail.com>
2025-10-31 19:34:31 +0200

    Update and rename firebase-messaging-sw.js to service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,10 @@ importScripts("https://www.gstatic.com/firebasejs/10.5.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "BH1rxDUeea6grr0gIXTI19kSWTMa50nHwu-GIShvIBKBndborG647a1A4xnHJ8KjB8iQkn5SuLbIOKdm_PXqIV8",
+  projectId: "modypoems",
+  messagingSenderId: "805060440847",
+  appId: "1:805060440847:web:bc4f89cd351c9c244f447a",
 });
 
 const messaging = firebase.messaging();
